e0ef768a3525e47cd5f5dbd5be73472456e37d94
Robby <buckdestructor@gmail.com>
2023-08-14 02:54:54 -0400

    Updated the bot to respond from a mention, rather than a generic command

--- test.py
+++ test.py
@@ -14,11 +14,9 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+async def on_message(message): # When the bot sees a message in the server
+    if str(client.user.id) in message.content: # If the bot is mentioned
+        if 'goat' in message.content: # If the message contains 'goat'
+            await message.channel.send('Mao Zedong is the Goat!') # Send a message in the same channel
 
 client.run('MTE0MDA2MjIzMTI4MzkwMDQ4Nw.GHm7ho.BfgWwqJgXVdZgFtMVla665S435yWE7Fm77YXaw')
