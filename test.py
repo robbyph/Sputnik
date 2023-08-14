c17f0d62117f78eb33497207a7cb69e581018309
Robby <buckdestructor@gmail.com>
2023-08-14 16:20:52 -0400

    Made it so the bot will respond regardless of letter case

--- test.py
+++ test.py
@@ -13,10 +13,12 @@ async def on_ready(): # When the bot is ready
     print(f'We have logged in as {client.user}')  # Print the bot's username
 
 
-@client.event
+@client.event 
 async def on_message(message): # When the bot sees a message in the server
     if str(client.user.id) in message.content: # If the bot is mentioned
-        if 'goat' in message.content: # If the message contains 'goat'
+        if 'goat' in message.content.lower(): # If the message contains 'goat'
             await message.channel.send('Mao Zedong is the Goat!') # Send a message in the same channel
+        if 'help' in message.content.lower(): # If the message contains 'help'
+            await message.channel.send('In order to prompt the bot, be sure to mention it with the "@" mention symbol. \n\nUtilize the following functions:\n* If you wish to know who the GOAT is, include "goat" in your message, or use the command "!goat"') # Send a message in the same channel
 
-client.run('MTE0MDA2MjIzMTI4MzkwMDQ4Nw.GHm7ho.BfgWwqJgXVdZgFtMVla665S435yWE7Fm77YXaw')      # Run the bot with the token
+client.run('MTE0MDA2MjIzMTI4MzkwMDQ4Nw.GHm7ho.BfgWwqJgXVdZgFtMVla665S435yWE7Fm77YXaw')  # Run the bot with the token
